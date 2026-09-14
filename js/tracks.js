3e39d1316e556fc5b01bc0067b5eda589efb98f6
northwesttrinity <allbackhome@gmail.com>
2026-09-15 06:43:58 +1000

    Add files via upload

--- js/tracks.js
+++ js/tracks.js
@@ -17,40 +17,40 @@ const ALBUMS = {
     year: "2002",
     cover: "images/album-cover-discounted-lucky-stars.png",
   },
-  "Seventeen Minute Hole": {
+  "Enter the Daydream": {
     year: "2004",
-    cover: "images/album-cover-seventeen-minute-hole.png",
+    cover: "images/album-cover-enter-the-daydream.png",
   },
-  "Just Visiting": {
+  "Seventeen Minute Hole": {
     year: "2006",
-    cover: "images/album-cover-just-visiting.png",
+    cover: "images/album-cover-seventeen-minute-hole.png",
   },
 };
 
 const TRACKS = [
-  // ---- Just Visiting, 2006 ----
-  { title: "First Day", album: "Just Visiting", src: "audio/track-05-first-day.mp3" },
-  { title: "Test Run", album: "Just Visiting", src: "audio/track-20-test-run.mp3" },
-  { title: "Black Hole", album: "Just Visiting", src: "audio/track-08-black-hole.mp3" },
-  { title: "Sink or Swim", album: "Just Visiting", src: "audio/track-19-sink-or-swim.mp3" },
-  { title: "Human Being", album: "Just Visiting", src: "audio/track-14-human-being.mp3" },
-  { title: "Weary Wanderers", album: "Just Visiting", src: "audio/track-18-weary-wanderers.mp3" },
-  { title: "Here I am Home", album: "Just Visiting", src: "audio/track-21-here-i-am-home.mp3" },
+  // ---- Seventeen Minute Hole, 2006 ----
+  { title: "Sink or Swim", album: "Seventeen Minute Hole", src: "audio/track-19-sink-or-swim.mp3" },
+  { title: "First Day", album: "Seventeen Minute Hole", src: "audio/track-05-first-day.mp3" },
+  { title: "Test Run", album: "Seventeen Minute Hole", src: "audio/track-20-test-run.mp3" },
+  { title: "Any Time You Like", album: "Seventeen Minute Hole", src: "audio/track-01-any-time-you-like.mp3" },
+  { title: "Human Being", album: "Seventeen Minute Hole", src: "audio/track-14-human-being.mp3" },
+  { title: "Weary Wanderers", album: "Seventeen Minute Hole", src: "audio/track-18-weary-wanderers.mp3" },
+  { title: "Here I am Home", album: "Seventeen Minute Hole", src: "audio/track-21-here-i-am-home.mp3" },
 
-  // ---- Seventeen Minute Hole, 2004 ----
-  { title: "Spin", album: "Seventeen Minute Hole", src: "audio/track-15-spin.mp3" },
-  { title: "Under One Sun", album: "Seventeen Minute Hole", src: "audio/track-09-under-one-sun.mp3" },
-  { title: "Breaking Out", album: "Seventeen Minute Hole", src: "audio/track-16-breaking-out.mp3" },
-  { title: "Round & Round", album: "Seventeen Minute Hole", src: "audio/track-13-round-and-round.mp3" },
-  { title: "We Are Change", album: "Seventeen Minute Hole", src: "audio/track-04-we-are-change.mp3" },
-  { title: "Giants & Gnomes", album: "Seventeen Minute Hole", src: "audio/track-12-giants-and-gnomes.mp3" },
-  { title: "The Trees Believe", album: "Seventeen Minute Hole", src: "audio/track-10-the-trees-believe.mp3" },
+  // ---- Enter the Daydream, 2004 ----
+  { title: "Spin", album: "Enter the Daydream", src: "audio/track-15-spin.mp3" },
+  { title: "Under One Sun", album: "Enter the Daydream", src: "audio/track-09-under-one-sun.mp3" },
+  { title: "Breaking Out", album: "Enter the Daydream", src: "audio/track-16-breaking-out.mp3" },
+  { title: "Round & Round", album: "Enter the Daydream", src: "audio/track-13-round-and-round.mp3" },
+  { title: "Giants & Gnomes", album: "Enter the Daydream", src: "audio/track-12-giants-and-gnomes.mp3" },
+  { title: "We Are Change", album: "Enter the Daydream", src: "audio/track-04-we-are-change.mp3" },
+  { title: "The Trees Believe", album: "Enter the Daydream", src: "audio/track-10-the-trees-believe.mp3" },
 
   // ---- Discounted Lucky Stars, 2002 ----
-  { title: "Any Time You Like", album: "Discounted Lucky Stars", src: "audio/track-01-any-time-you-like.mp3" },
-  { title: "The Evening", album: "Discounted Lucky Stars", src: "audio/track-06-the-evening.mp3" },
   { title: "Parting Breath", album: "Discounted Lucky Stars", src: "audio/track-17-parting-breath.mp3" },
   { title: "We Can Be Free", album: "Discounted Lucky Stars", src: "audio/track-02-we-can-be-free.mp3" },
+  { title: "The Evening", album: "Discounted Lucky Stars", src: "audio/track-06-the-evening.mp3" },
+  { title: "Black Hole", album: "Discounted Lucky Stars", src: "audio/track-08-black-hole.mp3" },
   { title: "All I Want", album: "Discounted Lucky Stars", src: "audio/track-07-all-i-want.mp3" },
   { title: "Light of the Lion", album: "Discounted Lucky Stars", src: "audio/track-03-light-of-the-lion.mp3" },
   { title: "Storm", album: "Discounted Lucky Stars", src: "audio/track-11-storm.mp3" },
